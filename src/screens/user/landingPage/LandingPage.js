84d8fe38fb039d8cdff9bce4ee89b67235507d1e
Effiong Bassey <f.young.de@gmail.com>
2021-02-10 03:12:08 +0100

    Added link to admin dashboard
    Addd link to redirect back to admin dashboard from homepage for admin users only

--- src/screens/user/landingPage/LandingPage.js
+++ src/screens/user/landingPage/LandingPage.js
@@ -2,9 +2,11 @@ import { useSelector } from 'react-redux'
 import Navbar from '../../../components/Navbar/Navbar'
 import SearchInput from '../../../components/search/SearchInput'
 import BusinessCard from '../../../components/businessCard/BusinessCard'
+import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
   const businessList = useSelector((state) => state.businessList)
+  const user = useSelector((state) => state.userLogin.userDetails)
 
   const displayBusinessList = businessList.map((business) => (
     <BusinessCard key={business.id} businessInfo={business} />
@@ -17,8 +19,13 @@ const LandingPage = () => {
         <SearchInput />
       </div>
       {displayBusinessList.length === 0 ? (
-        <div className='d-flex justify-content-center align-items-center'>
+        <div className=' text-center'>
           <p>No business list to display at this time.</p>
+          {user.hasOwnProperty('isAdmin') && (
+            <p className='mt-5'>
+              <Link to='/admin/dashboard'>Back to admin dashboard</Link>
+            </p>
+          )}
         </div>
       ) : (
         <div className='d-flex flex-wrap'>{displayBusinessList}</div>
